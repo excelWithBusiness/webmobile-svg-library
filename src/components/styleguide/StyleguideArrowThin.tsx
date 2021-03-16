34a9bc92c54d02ddeed03b05fe3e97a2ee389d2b
Matthew Hill <matthill8286@gmail.com>
2021-03-16 10:26:33 +0200

    feat: svgs updated

--- src/components/styleguide/StyleguideArrowThin.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import getSvgComponent from '../../utils/GetSvgComponent'
-
-export default getSvgComponent({
-  filteredPaths: <path d="M24 79.367h4.087l28.345-39.683L28.087 0H24l22.676 39.684z" />,
-  danonePaths: <path d="M24 79.367h4.087l28.345-39.683L28.087 0H24l22.676 39.684z" />,
-})
--- src/components/styleguide/StyleguideRightArrow.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import getSvgComponent from '../../utils/GetSvgComponent'
-
-export default getSvgComponent({
-  filteredPaths: <path d="M94.35 0l-35.7 35.7L175.95 153 58.65 270.3l35.7 35.7 153-153z" />,
-  danonePaths: <path d="M94.35 0l-35.7 35.7L175.95 153 58.65 270.3l35.7 35.7 153-153z" />,
-})
