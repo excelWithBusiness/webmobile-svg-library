f59e10e51f427a7254aa0085a0ab2196e9c6f030
Matthew Hill <matthill8286@gmail.com>
2021-03-23 20:15:28 +0200

    build: svgs version

--- src/components/styleguide/StyleguideMenuHamburger.tsx
+++ src/components/styleguide/StyleguideMenuHamburger.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import getSvgComponent from '../../utils/GetSvgComponent'
 
 export default getSvgComponent({
-  filteredPaths: (
-    <path fill="#F2F3F4" fillRule="evenodd" d="M36 4H0V0h36v4zm0 8H0v4h36v-4zm0 16v-4H0v4h36z" />
-  ),
-  danonePaths: (
-    <path fill="#F2F3F4" fillRule="evenodd" d="M36 4H0V0h36v4zm0 8H0v4h36v-4zm0 16v-4H0v4h36z" />
-  ),
+  filteredPaths: <path fillRule="evenodd" d="M36 4H0V0h36v4zm0 8H0v4h36v-4zm0 16v-4H0v4h36z" />,
+  danonePaths: <path fillRule="evenodd" d="M36 4H0V0h36v4zm0 8H0v4h36v-4zm0 16v-4H0v4h36z" />,
 })
