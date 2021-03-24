5f79462a58711c564c5f0fce3db3a2bc4c255195
Matthew Hill <matthill8286@gmail.com>
2021-03-24 11:07:59 +0200

    build: iconns added

--- src/components/styleguide/StyleguideVideo.tsx
+++ src/components/styleguide/StyleguideVideo.tsx
@@ -10,7 +10,7 @@ export default getSvgComponent({
           id="a"
         />
       </defs>
-      <use xlinkHref="#a" fillRule="evenodd" />
+      <use xlinkHref="#a" stroke-width="none" fillRule="evenodd" />
     </React.Fragment>
   ),
   danonePaths: (
@@ -21,7 +21,7 @@ export default getSvgComponent({
           id="a"
         />
       </defs>
-      <use xlinkHref="#a" fillRule="evenodd" />
+      <use xlinkHref="#a" stroke-width="none" fillRule="evenodd" />
     </React.Fragment>
   ),
 })
