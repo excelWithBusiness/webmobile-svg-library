78dbd0635e98cfb3c9bf90995a25a0f1de5b5d60
Matthew Hill <matthill8286@gmail.com>
2021-05-07 09:58:42 +0300

    feat: thickening

--- src/components/icon/IconArrowRightCircle.tsx
+++ src/components/icon/IconArrowRightCircle.tsx
@@ -5,11 +5,18 @@ function SvgComponent(props: React.SVGProps<SVGSVGElement>) {
     <svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
-        stroke="#999"
+        stroke="#fff"
+        strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      <path d="M12 16l4-4-4-4M8 12h8" stroke="#999" strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M12 16l4-4-4-4M8 12h8"
+        stroke="#fff"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   )
 }
--- src/components/styleguide/StyleguideArrowRightCircle.tsx
+++ src/components/styleguide/StyleguideArrowRightCircle.tsx
@@ -6,13 +6,15 @@ export default getSvgComponent({
     <React.Fragment>
       <path
         d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
-        stroke="#999"
+        stroke="#fff"
+        stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M12 16l4-4-4-4M8 12h8"
-        stroke="#999"
+        stroke="#fff"
+        stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
@@ -22,13 +24,15 @@ export default getSvgComponent({
     <React.Fragment>
       <path
         d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
-        stroke="#999"
+        stroke="#fff"
+        stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M12 16l4-4-4-4M8 12h8"
-        stroke="#999"
+        stroke="#fff"
+        stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
